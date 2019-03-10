653815c21e13aaaa72cc3c0f36e56ef9f553d9e1
williambiao <ziyuan.wang@mail.mcgill.ca>
2019-03-09 22:22:52 -0500

    upload file

--- src/test/java/StepDefinition/Steps.java
+++ src/test/java/StepDefinition/Steps.java
@@ -1,5 +1,9 @@
 package StepDefinition;		
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
 import org.openqa.selenium.By;		
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -44,14 +48,30 @@ public class Steps {
         if(ifInbox) {
         	driver.get("https://mail.google.com/mail/#inbox?compose=new");
         }
+        
         WebElement to = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.name("to")));
         to.sendKeys("williamin18@gmail.com");
         
         WebElement subject = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.name("subjectbox")));
         subject.sendKeys("ecse 428 a2 test");
         
-        WebElement send = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[text()='Send']")));
-        send.click();
+        WebElement attach = driver.findElement(By.cssSelector("div[class='a1 aaA aMZ']"));
+        attach.click();
+     
+        try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        type(4);
+       new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.name("attach")));
+
+        
+       WebElement send = (new WebDriverWait(driver, 20)).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[text()='Send']")));
+       send.click();
+        
+
     }		
 
     @Then("^Reset the credential$")					
@@ -59,5 +79,40 @@ public class Steps {
     {		
     	//driver.findElement(By.name("btnReset")).click();
     	driver.close();
-    } 
+    }
+    
+    private void type(int fileID) {//type different file name
+    	try {
+			Robot r = new Robot();
+			r.keyPress(KeyEvent.VK_SHIFT);
+			r.keyPress(KeyEvent.VK_C);
+			r.keyRelease(KeyEvent.VK_C);
+			r.keyPress(KeyEvent.VK_SEMICOLON);
+			r.keyRelease(KeyEvent.VK_SEMICOLON);
+			r.keyRelease(KeyEvent.VK_SHIFT);
+			r.keyPress(KeyEvent.VK_BACK_SLASH);
+			r.keyRelease(KeyEvent.VK_BACK_SLASH);
+			r.keyPress(KeyEvent.VK_A);
+			r.keyRelease(KeyEvent.VK_A);
+			r.keyPress(KeyEvent.VK_2);
+			r.keyRelease(KeyEvent.VK_2);
+			r.keyPress(KeyEvent.VK_BACK_SLASH);
+			r.keyRelease(KeyEvent.VK_BACK_SLASH);
+			r.keyPress(KeyEvent.VK_0+fileID);
+			r.keyRelease(KeyEvent.VK_0+fileID);
+			r.keyPress(KeyEvent.VK_PERIOD);
+			r.keyRelease(KeyEvent.VK_PERIOD);
+			r.keyPress(KeyEvent.VK_J);
+			r.keyRelease(KeyEvent.VK_J);
+			r.keyPress(KeyEvent.VK_P);
+			r.keyRelease(KeyEvent.VK_P);
+			r.keyPress(KeyEvent.VK_G);
+			r.keyRelease(KeyEvent.VK_G);
+			r.keyPress(KeyEvent.VK_ENTER);
+			r.keyRelease(KeyEvent.VK_ENTER);
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
 }		
